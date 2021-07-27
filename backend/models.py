5cefe3ea638404ef3c870fcab9a7864b52730080
Osama <osamahasanone@gmail.com>
2021-07-27 21:50:23 +0300

    rename relation table name between user and skill

--- backend/models.py
+++ backend/models.py
@@ -26,18 +26,18 @@ class SkillsResponse(BaseModel):
     items: List[SkillSchema]
 
 
-skills = db.Table('skills',
-                  db.Column('skill_id', db.Integer, db.ForeignKey(
-                      'skill.id'), primary_key=True),
-                  db.Column('user_id', db.Integer, db.ForeignKey(
-                      'user.id'), primary_key=True)
-                  )
+users_skills = db.Table('users_skills',
+                        db.Column('skill_id', db.Integer, db.ForeignKey(
+                            'skill.id'), primary_key=True),
+                        db.Column('user_id', db.Integer, db.ForeignKey(
+                            'user.id'), primary_key=True)
+                        )
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    skills = db.relationship('Skill', secondary=skills, lazy='subquery',
+    skills = db.relationship('Skill', secondary=users_skills, lazy='subquery',
                              backref=db.backref('users', lazy=True))
 
     def __repr__(self):
