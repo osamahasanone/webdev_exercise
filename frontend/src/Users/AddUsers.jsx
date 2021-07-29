24a91c390c7c387b47ffc2c1f2885fcc2c975476
Osama <osamahasanone@gmail.com>
2021-07-30 01:32:42 +0300

    frontend refactoring

--- frontend/src/Users/AddUsers.jsx
+++ frontend/src/Users/AddUsers.jsx
@@ -12,16 +12,3 @@ export default function AddUsers({ refetch }) {
   }, [refetch]);
   return <button onClick={onClick}>Add Users</button>;
 }
-
-const addSkillsBulk = async () => {
-  await fetch("http://127.0.0.1:5000/skills", {
-    method: "POST",
-  });
-};
-
-export function AddSkills({ refetch }) {
-  const onClick = useCallback(() => {
-    addSkillsBulk().then(refetch);
-  }, [refetch]);
-  return <button onClick={onClick}>Add Skills</button>;
-}
